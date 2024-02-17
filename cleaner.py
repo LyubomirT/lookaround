8a2c3ee20a0d5514911a769fbfce86d75ca33016
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-02-17 10:35:53 +0200

    Update .gitignore and cleaner.py, and fix panorama display in preview.html

--- cleaner.py
+++ cleaner.py
@@ -2,18 +2,14 @@ import os
 import shutil
 
 def clean_folder(folder, delete=False, ignore_gitkeep=True):
+    # The delete parameter is used to delete the folder instead of deleting all its contents
+    # The ignore_gitkeep parameter is used to ignore the .gitkeep file
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
-        if os.path.isfile(file_path):
-            if ignore_gitkeep and filename == '.gitkeep':
-                continue
-            if delete:
-                os.remove(file_path)
-            else:
-                shutil.move(file_path, os.path.join('static', filename))
-        else:
-            if delete:
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path) and not (ignore_gitkeep and filename == '.gitkeep'):
                 shutil.rmtree(file_path)
-            else:
-                shutil.move(file_path, os.path.join('static', filename))
-    return True
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
