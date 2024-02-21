074d38cd5e6c2e542f87953843e8d2cb7ea3ada0
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-02-21 20:13:18 +0200

    make it bun dem

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import os
 from cleaner import clean_folder
-
+import asyncio
+import schedule
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+def clean_folder_async():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(clean_folder(app.config['UPLOAD_FOLDER'], delete=False, ignore_gitkeep=True))
+
+def schedule_cleaning():
+    schedule.every(1).hours.do(clean_folder_async)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/clean')
-def clean():
-    clean_folder(app.config['UPLOAD_FOLDER'], delete=False, ignore_gitkeep=True)
-    return 'Folder cleaned'
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -26,5 +30,5 @@ def upload():
             return render_template('index.html', filename=filename)
 
 if __name__ == '__main__':
-    clean_folder(app.config['UPLOAD_FOLDER'], delete=False, ignore_gitkeep=True)
+    schedule_cleaning()
     app.run(debug=True, port=5000)
